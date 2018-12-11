f79e91d82b33bfa97713d4fdfbafba97d4f74ea8
qqqy <frogasaurus@hotmail.com>
2018-12-11 14:17:06 -0700

    added classlist routing

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import React from 'react'
 import {Switch, Route} from 'react-router-dom'
 import Home from './components/Home/Home.js'
 import About from './components/About/About.js'
+import ClassList from './components/ClassList/ClassList'
 
 export default (
   <Switch>
     <Route component={Home} path="/" exact />
     <Route component={About} path="/about" />
+    <Route component={ClassList} path="/classlist/:class" />
   </Switch>
 )

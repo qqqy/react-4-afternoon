ad2a15e06b6110c7e4287ece35f7c14a934a12ac
qqqy <frogasaurus@hotmail.com>
2018-12-11 13:42:06 -0700

    routes.js created

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import React from react
+import {Switch, Route} from 'react-router-dom'
+import Home from './components/Home/Home.js'
+import About from './components/About/About.js'
+
+export default (
+  <Switch>
+    <Route component={Home} path="/" exact />
+    <Route component={About} path="/about" />
+  </Switch>
+)

c8d50faf713f4a3de62255ba3003f01ede73b1ed
qqqy <frogasaurus@hotmail.com>
2018-12-11 14:39:04 -0700

    added student requests

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 export default class ClassList extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
+    this.state = {
+      students: []
+    }
     
   }
 
+  componentDidMount(){
+    axios.get(`http://localhost:3005/students?class=${this.props.match.params.class}`)
+      .then((res) => this.updateStudents(res.data))
+  }
+
+  updateStudents(data){
+    this.setState({students: data})
+    console.log(data)
+  }
+
   render() {
+    let students = this.state.students.map((student, i) => <h3 key={i}>{student.last_name}, {student.first_name}</h3>)
     return (
       <div className="box">
         <h1></h1>
         <h2>ClassList:</h2>
+        {students}
 
       </div>
     )

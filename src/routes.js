14922b931c965a70472bc55929542d050d651cc7
qqqy <frogasaurus@hotmail.com>
2018-12-11 14:04:20 -0700

    added links in App

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import React from react
+import React from 'react'
 import {Switch, Route} from 'react-router-dom'
 import Home from './components/Home/Home.js'
 import About from './components/About/About.js'
